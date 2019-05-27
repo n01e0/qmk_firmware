0933a4d27a63d503fb83d4039e749ab291f3d51f
n01e0 <noleo@vanillabeans.mobi>
2019-05-28 08:45:39 +0900

    swap ESC LSFT

--- keyboards/numatreus/keymaps/n01e0/keymap.c
+++ keyboards/numatreus/keymaps/n01e0/keymap.c
@@ -16,14 +16,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-        KC_ESC, KC_LSFT, KC_LGUI,  MO(_LW), KC_LCTL, KC_TAB, KC_ENT, KC_SPC,  MO(_RS), KC_RALT, KC_QUOT, KC_BSPC
+        KC_LSFT, KC_ESC, KC_LGUI,  MO(_LW), KC_LCTL, KC_TAB, KC_ENT, KC_SPC,  MO(_RS), KC_RALT, KC_QUOT, KC_BSPC
   ),
 
   /*
    *  1       2     3     4     5        ||     6    7     8     9    0
    *  #     left   down  right  $        ||     pgdn    4     5     6    +
    *  [       ]      (     )    &        ||       `     1     2     3    \
-   * lower  insert super shift space bksp|| alt Ent   fn    .     0    =
+   * lower  insert super shift ctrl  Tab || Ent Space   fn    .     0    =
    */
   [_RS] = LAYOUT( /* [> RAISE <] */
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,   KC_9, KC_0 ,
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN  ,
     KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN,                   KC_MINS, KC_PLUS,   KC_BSLS,   KC_LBRC,   KC_RBRC ,
     KC_VOLD,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_EQL,   KC_F1,   KC_F2,   KC_UP,   KC_BSLS ,
-    KC_ESC,   KC_LSFT, KC_LGUI, KC_LSFT, KC_LCTL, KC_TAB, KC_ENT, KC_SPC,  TO(_QW), KC_LEFT, KC_DOWN, KC_RGHT )
+    KC_LSFT,   KC_ESC, KC_LGUI, KC_LSFT, KC_LCTL, KC_TAB, KC_ENT, KC_SPC,  TO(_QW), KC_LEFT, KC_DOWN, KC_RGHT )
 
 };
 
