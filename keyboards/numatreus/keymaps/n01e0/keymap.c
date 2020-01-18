bdf3dcafb94d302e11f2382f2ca4afdab492f89b
n01e0 <noleo@vanillabeans.mobi>
2020-01-19 04:14:56 +0900

    numatreus configured

--- keyboards/numatreus/keymaps/n01e0/keymap.c
+++ keyboards/numatreus/keymaps/n01e0/keymap.c
@@ -12,11 +12,16 @@
 #define _LW 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_QW] = LAYOUT( /* Qwerty */
+  [_QW] = LAYOUT( 
+  /* Qwerty 
+   * Q        W        E        R        T                            Y        U        I       O       P
+   * A        S        D        F        G                            H        J        K       L       ;
+   * Z        X        C        V        B                            N        M        ,       .       /
+   * LShift   ESC      SUPER    LCtrl    Lower  Space      Enter      Raise    Tab      LAlt    Quot    BS*/
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-        KC_LSFT, KC_ESC, KC_LGUI,  MO(_LW), KC_LCTL, KC_TAB, KC_ENT, KC_SPC,  MO(_RS), KC_RALT, KC_QUOT, KC_BSPC
+    KC_LSFT, KC_ESC, KC_LGUI,  KC_LCTL, MO(_RS), KC_SPC,    KC_ENT, MO(_LW), KC_TAB,  KC_RALT, KC_QUOT, KC_BSPC
   ),
 
   /*
@@ -40,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LW] = LAYOUT( /* [> LOWER <] */
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN  ,
     KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN,                   KC_MINS, KC_PLUS,   KC_BSLS,   KC_LBRC,   KC_RBRC ,
-    KC_VOLD,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_EQL,   KC_F1,   KC_F2,   KC_UP,   KC_BSLS ,
+    KC_VOLD,   KC_VOLU, KC_F3,   KC_F5,   RESET,                     KC_EQL,   KC_F1,   KC_F2,   KC_UP,   KC_BSLS ,
     KC_LSFT,   KC_ESC, KC_LGUI, KC_LSFT, KC_LCTL, KC_TAB, KC_ENT, KC_SPC,  TO(_QW), KC_LEFT, KC_DOWN, KC_RGHT )
 
 };
