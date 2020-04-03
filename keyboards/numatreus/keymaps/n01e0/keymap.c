0affc4a0c0873a31700fe676b681c1afe33390a3
n01e0 <noleo@vanillabeans.mobi>
2020-04-03 13:12:12 +0900

    f12

--- keyboards/numatreus/keymaps/n01e0/keymap.c
+++ keyboards/numatreus/keymaps/n01e0/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_LSFT, KC_ESC, KC_LGUI,  KC_LCTL, MO(_RS), KC_SPC,    KC_ENT, MO(_LW), KC_TAB,  KC_RALT, KC_QUOT, KC_BSPC
+    KC_LSFT, KC_ESC, KC_LGUI,  KC_LCTL, MO(_LW), KC_SPC,    KC_ENT, MO(_RS), KC_TAB,  KC_RALT, KC_QUOT, KC_BSPC
   ),
 
   /*
@@ -30,10 +30,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *  [       ]      (     )    &        ||       `     1    [     ]    \
    * lower insert super shift ctrl super || Ent Space   fn    .     +    BS
    */
-  [_RS] = LAYOUT( /* [> RAISE <] */
+   [_LW] = LAYOUT( /* [> LOWER <] */
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,   KC_9, KC_0 ,
     KC_HASH, KC_LEFT, KC_PGUP, KC_PGDN, KC_DLR,                    KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT, KC_PLUS ,
-    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_1,    KC_LBRC,   KC_RBRC, KC_BSLS ,
+    KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_F12,    KC_LBRC,   KC_RBRC, KC_BSLS ,
     TG(_LW), KC_INS,  KC_LGUI, KC_LCTL, KC_LCTL, KC_LGUI, KC_ENT, KC_SPC,  KC_TRNS, KC_DOT, KC_EQL, KC_BSPC  ),
 
   /*
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *       volup             reset      ||     =    F1    F2    F3   F12
    *       voldn  super shift space bspc|| alt  ent    L0  prtsc scroll pause
    */
-  [_LW] = LAYOUT( /* [> LOWER <] */
+  [_RS] = LAYOUT( /* [> RAISE <] */
     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN  ,
     KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN,                   KC_MINS, KC_PLUS,   KC_BSLS,   KC_LBRC,   KC_RBRC ,
     KC_VOLD,   KC_VOLU, KC_F3,   KC_F5,   RESET,                     KC_EQL,   KC_F1,   KC_F2,   KC_UP,   KC_BSLS ,
